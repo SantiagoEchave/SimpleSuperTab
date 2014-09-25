49888f008a7d145997a55da66018ec63280ae02a
Santiago Echave <santiagomiguelechave@MacBook-Pro-de-Santiago.local>
2014-09-25 19:20:44 -0300

    first commit

---
+++ js/function.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+
+    
+    
+});
+//    
+
+
+//Ocultar el Ul
+//    $("ul").hide();
+//    //Apuntar en el DOM click img
+//    $(".pete").click(function(e){
+//        e.preventDefault();
+//        $("ul").slideToggle();
+//    });
+//
+//
+////    $(".FirstCont").prepend("<p>Ahhh sos normal?</p>");
+//    $(".FirstCont").append("<p>Ahhh sos normal?</p>");
+
+
+
+
+//var light = $(".transparencia, .box_principal");
+//    light.hide();
+//    $(".open_button").click(function(e){
+//        e.preventDefault();
+//        light.fadeIn();
+//    });
+//    $(".click").click(function(e){
+//        e.preventDefault();
+//        light.fadeOut();
+//    });
+//    var anchoVentana =$(window).width();
+//    var altoVentana = $(window).height();
+//    console.log(anchoVentana,altoVentana);
+//    var anchoModal = $(".box_principal").width();
+//    var altoModal = $(".box_principal").height();
+//    console.log(anchoModal, altoModal);
+//    var posicionSide = (anchoVentana-anchoModal)/2;
+//    console.log(posicionSide)
+//    var posicionHeight = (altoVentana-altoModal)/2;
+//    
+//    $(".box_principal").css("top",posicionHeight);
+//    $(".box_principal").css("left",posicionSide);
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
